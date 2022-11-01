a25a247ca110822d92468b945364f42442bc7a90
BrandonCarlos <brandon_marzo@hotmail.com>
2022-11-01 14:24:03 -0300

    add fake full rest api using json-server to serve data in application

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -7,14 +7,21 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
-  @Input() pensamento = {
-    conteudo: 'I love Angular',
+  @Input() pensamento = {//array = "listarPensamento e pensamento" are the same thing, what change is the name only
+    conteudo: 'I love Angular',//therefore why have these fields = conteudo, autoria and modelo in 2 file
     autoria: 'Brandon',
     modelo: 'modelo3'
   }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  larguraPensamento(): string {//function why return a class different
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
+    }
+    return 'pensamento-p';
   }
 
 }
